8473d061fd2b23c5b5f4ea8dd5df1a37b2fc1142
hridayjham <jhamh@mcmaster.ca>
2022-08-07 17:54:57 +0530

    Adding project files

---
+++ TwitterClone/ Core/Components/Tweets/TweetRowView.swift
@@ -0,0 +1,20 @@
+//
+//  TweetRowView.swift
+//  TwitterClone
+//
+//  Created by Hriday Jham on 2022-08-07.
+//
+
+import SwiftUI
+
+struct TweetRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TweetRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        TweetRowView()
+    }
+}
---
+++ TwitterClone/ Core/Feed/View/FeedView.swift
@@ -0,0 +1,28 @@
+//
+//  FeedView.swift
+//  TwitterClone
+//
+//  Created by Hriday Jham on 2022-08-07.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        
+        ScrollView {
+            LazyVStack {
+                ForEach(0 ... 20, id: \.self) { _ in
+                    Text("Tweet Row View")
+                }
+            }
+        }
+        
+    }
+}
+
+struct FeedView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedView()
+    }
+}
