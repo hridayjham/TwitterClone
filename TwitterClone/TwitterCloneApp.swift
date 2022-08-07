4868f5893363bb5f6c0edb92b8285c082e7bee6c
hridayjham <jhamh@mcmaster.ca>
2022-08-07 16:23:12 +0530

    Initial Commit

---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by Hriday Jham on 2022-08-07.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
